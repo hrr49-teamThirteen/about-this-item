6c71a838f36802233b71ca3728d9494d22db866e
Nicholas Mendini <nicholas.mendini@gmail.com>
2020-11-13 17:04:46 -0600

    Created stubs for all anticipated routes.

--- server/server.js
+++ server/server.js
@@ -8,6 +8,34 @@ const db = require('../db/mariadb.js');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// API ROUTES
+app.get('/api/products/:id/details ', (req, res) => {
+
+});
+
+app.get('/api/products/:id/questions', (req, res) => {
+
+});
+
+app.get('/api/products/:id/answers', (req, res) => {
+
+});
+
+app.post('/api/products/:id/questions', (req, res) => {
+
+});
+
+app.post('/api/products/:id/answers', (req, res) => {
+
+});
+
+app.put('/api/product/:id/helpful', (req, res) => {
+
+});
+
+app.put('/api/product/:id/not-helpful', (req, res) => {
+
+});
 
 let port = process.env.PORT || 5000;
 app.listen(port, () => {
