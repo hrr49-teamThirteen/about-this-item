92590a2e36319c593ba5be67d9fbaecb380ffd4c
Nicholas Mendini <nicholas.mendini@gmail.com>
2020-11-18 16:49:33 -0600

    Implemented QuestionList component

--- client/components/QuestionList.jsx
+++ client/components/QuestionList.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
+import QuestionListEntry from './QuestionListEntry.jsx';
 
 const QuestionList = function(props) {
   return (
-    <div>
-      <div><h3 className="question-row"><span>Q: </span><span className="question-text">Question goes here?</span></h3></div>
-      <div className="question-row-name"><span>Name</span> — Post Date Goes Here</div>
-      <div className="container-answer">
-        <div className="answer-row"><span className="answer-bold">A: </span><span>The point that pierces the pod is most likely clogged. There’s a tiny opening which needs to be cleared. I clean mine monthly by using a straightened paper clip. You can google how to clean your machine. Be sure to unplug your Keurig machine first.</span></div>
-        <div className="answer-row-name"><span>Name</span> — Post Date Goes Here <span className="helpful answer-click"> Helpful(number) </span><span className="not-helpful answer-click"> Not helpful(number) </span><span className="report-answer answer-click"> Report </span></div>
-        <div className="answer-row"><span className="answer-bold">A: </span><span>The point that pierces the pod is most likely clogged. There’s a tiny opening which needs to be cleared. I clean mine monthly by using a straightened paper clip. You can google how to clean your machine. Be sure to unplug your Keurig machine first.</span></div>
-        <div className="answer-row-name"><span>Name</span> — Post Date Goes Here <span className="helpful answer-click"> Helpful(number) </span><span className="not-helpful answer-click"> Not helpful(number) </span><span className="report-answer answer-click"> Report </span></div>
-      </div>
-      <button type="button" className="btn">Answer it</button>
-      <hr></hr>
+    <div>{
+      props.questions.map((item, i) =>
+        <QuestionListEntry question={item} key={i} answers={props.answers}/>
+      )}
     </div>
   );
 };
