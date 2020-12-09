aa0275499f8002b1976dfa8579804676ca2857dc
David Kim <davidhk21@gmail.com>
2020-12-09 15:13:40 -0700

    refactored data to be formatted on client side, UI works as expected

--- client/components/SpecificationsList.jsx
+++ client/components/SpecificationsList.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import SpecificationsListEntry from './SpecificationsListEntry.jsx';
 import styles from '../assets/style.css';
 
+import faker from 'faker';
+
 const SpecificationsList = function(props) {
   return (
     <div>
       <div className={styles.specContainer}>
         <b>Dimensions (Overall): </b>
-        {`${props.specifications['Height']} (H) x
+        {/* {`${props.specifications['Height']} (H) x
         ${props.specifications['Weight']} (W) x
-        ${props.specifications['Length']} (D)`}
+        ${props.specifications['Length']} (D)`} */}
+        {`${(Math.random() * 30).toFixed(2)} inches (H) x
+        ${(Math.random() * 30).toFixed(2)} inches (W) x
+        ${(Math.random() * 30).toFixed(2)} inches (D)`}
         <hr></hr>
       </div>
       <div className={styles.specContainer}>
-        <b>Weight:</b> {props.specifications['Weight']}
+        <b>Weight:</b> {`${(Math.random() * 20).toFixed(2)} pounds`}
         <hr></hr>
       </div>{
         Object.keys(props.specifications).map((item, i) => {
