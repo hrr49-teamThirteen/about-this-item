f24aca40394b239bfc3d6b6242f9fcae5c99f60f
David Kim <davidhk21@gmail.com>
2020-12-14 18:05:51 -0700

    brotli compression

--- webpack.config.js
+++ webpack.config.js
@@ -27,18 +27,9 @@ module.exports = {
       }],
   },
   plugins: [
-    new CompressionPlugin({
-      asset: '[path].gz[query]',
-      algorithm: `gzip`,
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.7,
-    }),
     new BrotliPlugin({
-      asset: '[path].br[query]',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.7,
+      asset: 'bundle.js.br',
+      test: /\.(js)$/,
     }),
   ],
 };
