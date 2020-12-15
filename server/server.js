c50a96fa3f6cc08608694fadc15ea0ba43dfc787
David Kim <davidhk21@gmail.com>
2020-12-14 18:21:47 -0700

    got rid of compression middleware for testing

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const app = express();
 const bodyParser = require('body-parser');
-const compression = require('compression');
+// const compression = require('compression');
 const db = require('./controllers/controllers.js');
 
 app.use(express.static(`${__dirname}/../public/`));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(compression());
+// app.use(compression());
 
 // ---------- COMPRESSION ---------- //
 
